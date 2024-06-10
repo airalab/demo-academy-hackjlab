a965922356a2d93a248654ea034d81915eb868c8
Anastasiia Bakai <nastin_box@list.ru>
2024-06-10 20:52:57 +0300

    fix hours

--- src/App.vue
+++ src/App.vue
@@ -254,13 +254,13 @@ const nextsession = computed( () => {
   const nowday = parseInt(new Date(Date.now()).toLocaleString('en-US', { timeZone: timezone, day: '2-digit' }));
   const nowhour = parseInt(new Date(Date.now()).toLocaleString('en-US', { timeZone: timezone, hour: '2-digit', hour12: false }));
 
-  if(nowday < 17 && nowhour < 20 ) {
+  if(nowday < 17 && nowhour <= 20 ) {
     if(nowhour >= 10 && nowhour < 20) {
       return new Date(Date.now()).toLocaleDateString('en-US', { timeZone: timezone, dateStyle: 'medium'}) + ' 20:00 GMT+3';
     } else {
       const day = new Date();
 
-      if(nowhour > 20) {
+      if(nowhour >= 20) {
         day.setDate(new Date().getDate() + 1);
       }
 
